2fe9f70a5614a39420d824583e6040b68cb81048
ChewChew123 <chewsaihou@gmail.com>
2026-03-28 16:17:41 +0800

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,1 +1,19 @@
-<?php // Assigned to [Member Name] ?>
+<?php
+
+$host = "localhost";       
+$username = "root";         
+$password = "";             
+$database = "co_curricular_db"; 
+
+
+$conn = new mysqli($host, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+   
+    die("Database Connection Failed: " . $conn->connect_error);
+}
+
+
+$conn->set_charset("utf8mb4");
+?>
